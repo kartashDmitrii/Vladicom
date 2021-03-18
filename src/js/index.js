e848a2155b452df91c1e5dff98f7525a0408d770
Kartash_Dmitry <kartaschdim@gmail.com>
2021-03-18 14:03:59 +0200

    18.03

--- src/js/index.js
+++ src/js/index.js
@@ -1,44 +1,70 @@
-if (document.querySelector('section.banner')){
-    let swiper = new Swiper('section.banner .swiper-container', {
-        centeredSlides: true,
-        loop: true,
-        autoplay: {
-            delay: 5000,
-            disableOnInteraction: false,
+import NumberTimer from "./components/NumberTimer";
+if (document.querySelector('section.top-block')){
+    let swiper = new Swiper('section.top-block .swiper-container',{
+        effect: 'fade',
+        pagination: {
+            el: 'section.top-block .custom-pagination',
+            clickable: true,
+            renderBullet(index, className){
+                return `<span class=${className}></span>`
+            }
         }
     });
 }
-if (document.querySelector('section.brands')){
-    document.querySelectorAll('section.brands').forEach( elem => {
-        let swiper = new Swiper(elem.querySelector('.swiper-container'), {
-            slidesPerView: 5,
-            slidesPerGroup: 5,
-            spaceBetween: 20,
-            pagination: {
-                el: elem.querySelector('.pagination'),
-                clickable: true,
-                renderBullet: function (index, className) {
-                    return `<span class="dot ${className}"></span>`
-                }
-            },
-            breakpoints: {
-                768: {
-                    slidesPerView: 5,
-                    slidesPerGroup: 5,
-                },
-                577: {
-                    slidesPerView: 4,
-                    slidesPerGroup: 4,
-                },
-                421: {
-                    slidesPerView: 3,
-                    slidesPerGroup: 3,
-                },
-                0: {
-                    slidesPerView: 2,
-                    slidesPerGroup: 2,
+if (document.querySelector('[data-aos]')){
+    AOS.init();
+}
+/*          FEATURES NUMBER        */
+
+if (document.querySelector('section.info-about-us')){
+    function scrollEvent(e){
+        if(pageYOffset > (document.querySelector('section.info-about-us').offsetTop - window.screen.height)){
+            document.querySelectorAll('section.info-about-us .count').forEach( elem => {
+                new NumberTimer({
+                    elem: elem.querySelector('span'),
+                    delay: 2500,
+                    type: 'ease-out'
+                })
+            });
+            window.removeEventListener('scroll', scrollEvent)
+        }
+    }
+    window.addEventListener('scroll', scrollEvent)
+
+}
+
+/*          FEATURES NUMBER        */
+
+/*          FOOTER BTN SCROLL TO    */
+
+if(document.querySelector('footer>a')){
+    document.querySelector('footer>a').addEventListener('click', ()=>{
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    })
+}
+
+/*          FOOTER BTN SCROLL TO    */
+
+if (document.querySelector('header nav>ul>li.has_child')){
+    document.querySelectorAll('header nav>ul>li.has_child').forEach( elem => {
+        let elemOffsetLeft = elem.parentNode.offsetLeft + elem.offsetLeft;
+        elem.querySelectorAll("li.has_child>ul").forEach( child => {
+            let childOffsetLeft = elemOffsetLeft;
+
+            let elem = child;
+            while (elem.parentNode.classList.contains('has_child') || elem.parentNode.classList.contains('sub_menu')){
+                elem = elem.parentNode;
+                if (elem.parentNode.classList.contains('sub_menu')){
+                    childOffsetLeft += elem.parentNode.offsetWidth;
                 }
             }
-        });
+            let childOffsetRight = window.screen.width - childOffsetLeft;
+            if(childOffsetRight - child.offsetWidth < 0){
+                child.classList.add('reverse');
+            }
+        })
     })
 }
